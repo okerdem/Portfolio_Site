bfcbb3700806905d20ae7849d03decfe61c89e32
odman <okerdem.13@gmail.com>
2023-10-17 19:17:30 +0300

    - - - - - - -
    feat: add light and dark theme. add navigation bar to left side.
    
    - - - - - - -

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+/* hamburger menu */
+
 function toggleMenu() {
-  /*get the first elements of the class in brackets to const value*/ 
+  /* get the first elements of the class in brackets to const value */
   const menu = document.querySelector(".menu_links");
   const icon = document.querySelector(".hamburger_icon");
-  /*on and off the "open" of elements with specified class*/ 
+  /* on and off the "open" of elements with specified class */
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+}
+
+function changeTheme(img) {
+  if (img.src.match(/sun_on/)) {
+    img.src = "./assets/sun_off.png";
+    document.body.style.backgroundColor = "gray";
+  } else {
+    img.src = "./assets/sun_on.png";
+    document.body.style.backgroundColor = "white";
+  }
 }
